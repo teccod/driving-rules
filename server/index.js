16b876d5267a8e964acfaface4159fa06c11cb8b
NjekTt <v.berkutovv@gmail.com>
2022-06-22 18:21:16 +0400

    Fix containers

--- server/index.js
+++ server/index.js
@@ -1,32 +1,29 @@
-require('dotenv').config()
-const express = require('express');
-const mysql = require('mysql2');
+require("dotenv").config();
+const express = require("express");
+const mysql = require("mysql2");
 
 const app = express();
 
 const connection = mysql.createPool({
-    connectionLimit: 5,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASSWORD
+	connectionLimit: 5,
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	database: process.env.DB_DATABASE,
+	password: process.env.DB_PASSWORD,
 });
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	next();
 });
 
-app.get('/api/getCountriesTable', function(req, res) {
+app.get("/api/getCountriesTable", function (req, res) {
+	let sql = "SELECT * FROM countries_rule";
 
-    let sql = "SELECT * FROM countries_rule";
-
-    connection.query(sql, function(err, result){
-        if(err)
-            res.status(500).send({'message' : err.message})
-        else
-            res.json(result)
-    })
+	connection.query(sql, function (err, result) {
+		if (err) res.status(500).send({ message: err.message });
+		else res.json(result);
+	});
 });
 
 app.listen(process.env.PORT);
